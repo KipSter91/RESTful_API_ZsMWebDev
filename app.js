c8bb99a604be998cf9c9e921fc7aa8f24cadb782
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-04-22 16:18:16 +0200

    add ".put"

--- app.js
+++ app.js
@@ -23,43 +23,77 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
+/////request targeting all articles/////
+
 app.route("/articles")
 
-.get((req, res) => {
-    Article.find({})
-        .then(foundItems => {
-            res.send(foundItems);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
-
-.post((req, res) => {
-
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+    .get((req, res) => {
+        Article.find({})
+            .then(foundArticles => {
+                res.send(foundArticles);
+            })
+            .catch(err => {
+                console.log(err);
+            });
     })
 
-    newArticle.save()
-        .then(() => {
-            res.send("Succesfully added a new article.")
-        })
-        .catch ((err) => {
-            console.log(err);
-        });
-})
+    .post((req, res) => {
 
-.delete((req, res) => {
-    Article.deleteMany({})
-    .then (() => {
-        res.send("All articles deleted.")
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+
+        newArticle.save()
+            .then(() => {
+                res.send("Succesfully added a new article.")
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     })
-    .catch ((err) => {
-        console.log(err);
+
+    .delete((req, res) => {
+        Article.deleteMany({})
+            .then(() => {
+                res.send("All articles deleted.")
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
-});
+
+/////request targeting a specific article/////
+
+app.route("/articles/:articleTitle")
+
+    .get((req, res) => {
+
+        Article.findOne({ title: req.params.articleTitle })
+            .then((foundArticle) => {
+                if (foundArticle) {
+                    res.send(foundItem)
+                } else {
+                    res.send("No articles matching that title was found.")
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    })
+
+    .put((req, res) => {
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+        )
+            .then(() => {
+                res.send("1 article is succesfully updated.")
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
