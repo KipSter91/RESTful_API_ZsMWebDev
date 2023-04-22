e1ab6239acc7d652dd23e1a092484a6251582b98
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-04-22 07:43:04 +0200

    fix problem with ejs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const ejs = require(ejs);
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 const port = "3000";
 
