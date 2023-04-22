6cc2ae1c2c500c59630069ea6f4c0d090eb24f76
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-04-22 14:46:58 +0200

    refactor code adding "app.route"

--- app.js
+++ app.js
@@ -18,19 +18,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/WikiDB', {
 
 const articleSchema = new mongoose.Schema({
     title: String,
-    contect: String
+    content: String
 });
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", (req, res) => {
+app.route("/articles")
+
+.get((req, res) => {
     Article.find({})
         .then(foundItems => {
-            console.log(foundItems);
+            res.send(foundItems);
         })
         .catch(err => {
             console.log(err);
         });
+})
+
+.post((req, res) => {
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    })
+
+    newArticle.save()
+        .then(() => {
+            res.send("Succesfully added a new article.")
+        })
+        .catch ((err) => {
+            console.log(err);
+        });
+})
+
+.delete((req, res) => {
+    Article.deleteMany({})
+    .then (() => {
+        res.send("All articles deleted.")
+    })
+    .catch ((err) => {
+        console.log(err);
+    });
 });
 
 app.listen(port, () => {
